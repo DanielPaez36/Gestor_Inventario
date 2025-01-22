0317e3845a105b6ca54f712b3de520bd5683e7d5
DanielPaez36 <rollerpaez36@gmail.com>
2025-01-22 14:21:05 -0500

    crud productos

--- apps/almacen/urls.py
+++ apps/almacen/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.almacen.views import categoria_views
+from apps.almacen.views import categoria_views, producto_views
 
 urlpatterns = [
 
@@ -12,4 +12,11 @@ urlpatterns = [
     path('eliminar_categoria/<int:id>/', categoria_views.eliminar_categoria, name='eliminar_categoria'),
     path('editar_categoria/<int:id>/', categoria_views.editar_categoria, name='editar_categoria'),
     path('procesar_editar_categoria/', categoria_views.procesar_editar_categoria, name='procesar_editar_categoria'),
+
+    #Productos
+    path('productos/', producto_views.productos, name='productos'),
+    path('crear_producto/', producto_views.crear_producto, name='crear_producto'),
+    path('eliminar_producto/<int:id>/', producto_views.eliminar_producto, name='eliminar_producto'),
+    path('editar_producto/<int:id>/', producto_views.editar_producto, name='editar_producto'),
+    path('procesar_editar_producto/', producto_views.procesar_editar_producto, name='procesar_editar_producto'),
 ]
---
+++ apps/almacen/views/producto_views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from ..models import Producto, Categoria
+
+def productos(request):
+    productos = Producto.objects.all()
+    categorias = Categoria.objects.all()
+    context = {
+        'productos':productos,
+        'categorias':categorias,
+    }
+    return render(request, 'producto/productos.html', context)
+
+def crear_producto(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre_prod']
+        descripcion = request.POST['descripcion_prod']
+        precio = request.POST['precio_prod']
+        stock = request.POST['stock_prod']
+        codigo_barras = request.POST['codigo_barras_prod']
+        fk_id_categoria = request.POST['fk_id_categoria']
+        categoria_selec = Categoria.objects.get(id=fk_id_categoria)
+        Producto.objects.create(
+            nombre_prod=nombre,
+            descripcion_prod=descripcion,
+            precio_prod=precio,
+            stock_prod=stock,
+            codigo_barras_prod=codigo_barras,
+            fk_id_categoria=categoria_selec
+        )
+        messages.success(request, 'Producto creado correctamente')
+        return redirect('productos')
+
+def eliminar_producto(request,id):
+    producto = Producto.objects.get(id=id)
+    producto.delete()
+    messages.success(request, 'Producto eliminado correctamente')
+    return redirect('productos')
+
+def editar_producto(request, id):
+    producto = Producto.objects.get(id=id)
+    categorias = Categoria.objects.all()
+    context = {
+        'producto':producto,
+        'categorias':categorias
+    }
+    return render(request, 'producto/editar_producto.html', context)
+
+def procesar_editar_producto(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        nombre = request.POST['nombre_prod']
+        descripcion = request.POST['descripcion_prod']
+        precio = request.POST['precio_prod']
+        stock = request.POST['stock_prod']
+        codigo_barras = request.POST['codigo_barras_prod']
+        fk_id_categoria = request.POST['fk_id_categoria']
+
+        categoria_selec = Categoria.objects.get(id=fk_id_categoria)
+        producto_editar = Producto.objects.get(id=id)
+        producto_editar.nombre_prod = nombre
+        producto_editar.descripcion_prod = descripcion
+        producto_editar.precio_prod = precio
+        producto_editar.stock_prod = stock
+        producto_editar.codigo_barras_prod = codigo_barras
+        producto_editar.fk_id_categoria = categoria_selec
+        producto_editar.save()
+        messages.success(request, 'Producto editado correctamente')
+        return redirect('productos')
